f4aa8feee808a5bf7b25f816517c4509acd70e0c
marco villanueva <villanuevamarco98@gmail.com>
2020-10-28 20:24:28 -0600

    Add project files.

---
+++ W3_Exercises/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace W3_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //This is a single line comment
+            /*This is a 
+             multi-line comment*/
+
+            //Create a variable named myNum and assign the value 50 to it.
+
+            int myNum = 50;
+
+            //Create a variable named myName and assign the value "John" to it.
+
+            string myName = "John";
+
+            //Display the sum of 5 + 10, using two variables: x and y.
+
+            int x = 5;
+            int y = 10;
+            Console.WriteLine(x + y);
+
+            //Create an int variable called z, assign x + y to it, and display the result.
+
+            int a = 5;
+            int b = 10;
+            int c = a + b;
+            Console.WriteLine(c);
+
+            //Fill in the missing parts to create three variables of the same type, using a comma-separated list:
+
+            int d = 5, e = 10, f = 2;
+            Console.WriteLine(d + e + f);
+
+        }
+    }
+}
